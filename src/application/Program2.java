2d562c38bbb7ee5950ab7fb09e38dac38a5b2dac
Thiago <thiagolima1011@gmail.com>
2021-01-27 19:03:26 -0300

    Department - FindAll implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,11 +1,13 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
-import model.dao.SellerDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program2 {
 
@@ -17,11 +19,15 @@ public class Program2 {
 		System.out.println("==== TESTE 1: Department findById ====");
 		Department department = departmentDao.findById(2);
 		System.out.println(department);
-		
-		/*
+				
 		System.out.println("\n==== TESTE 2: Department findAll ====");
-		
-		
+		List<Department> list = new ArrayList<Department>();
+		list = departmentDao.findAll();
+		for (Department obj : list) {
+			System.out.println(obj);
+		}
+				
+		/*
 		System.out.println("\n==== TESTE 3: Department insert ====");
 		
 		
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -70,12 +73,24 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public List<Department> findAll() {
-		
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement("SELECT * FROM DEPARTMENT");
+			rs = st.executeQuery();
+			List<Department> list = new ArrayList<>();
+			while (rs.next()) {
+				Department obj = new Department(); 
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				list.add(obj);
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
-	
-	
-	
-	//findById implementation
-
 }
