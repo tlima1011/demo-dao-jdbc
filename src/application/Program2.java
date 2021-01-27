55d506abd0cd859129fb62d2e4e451ddfa63e929
Thiago <thiagolima1011@gmail.com>
2021-01-27 19:52:08 -0300

    Department delete implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -13,14 +13,14 @@ import model.entities.Seller;
 public class Program2 {
 
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in); 
+
+		Scanner sc = new Scanner(System.in);
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
-		
+
 		System.out.println("==== TESTE 1: Department findById ====");
 		Department department = departmentDao.findById(2);
 		System.out.println(department);
-				
+
 		System.out.println("\n==== TESTE 2: Department findAll ====");
 		List<Department> list = new ArrayList<Department>();
 		list = departmentDao.findAll();
@@ -30,18 +30,20 @@ public class Program2 {
 		System.out.println("\n==== TESTE 3: Department insert ====");
 		Department newDepartment = new Department(null, "Sales");
 		departmentDao.insert(newDepartment);
-		System.out.println("Inserted!! New id = " +newDepartment.getId());		
-		
+		System.out.println("Inserted!! New id = " + newDepartment.getId());
+
 		System.out.println("\n==== TESTE 4: Department Update ====");
 		department = departmentDao.findById(6);
 		department.setName("Supply Chain");
 		departmentDao.update(department);
 		System.out.println("Update completed");
-		
-		/*
+
 		System.out.println("\n==== TESTE 5: Department delete ====");
-		*/
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
+
 		sc.close();
 	}
-
 }
